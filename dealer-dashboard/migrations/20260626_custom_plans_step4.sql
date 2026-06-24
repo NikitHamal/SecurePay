feb50b547556b93e6411a1ae6c2e540466605ca0
Nikit Hamal <iamnikithamal@gmail.com>
2026-06-24 19:38:19 +0545

    up

--- dealer-dashboard/migrations/20260626_custom_plans_step4.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE accounts_old;
-
-CREATE INDEX IF NOT EXISTS idx_accounts_dealer ON accounts(dealer_id);
-CREATE INDEX IF NOT EXISTS idx_accounts_status ON accounts(status);
-CREATE INDEX IF NOT EXISTS idx_accounts_phone  ON accounts(phone_number);
-CREATE INDEX IF NOT EXISTS idx_accounts_due    ON accounts(next_payment_due);
-CREATE INDEX IF NOT EXISTS idx_accounts_device_secret ON accounts(device_hmac_secret_created_at);
-
-PRAGMA foreign_keys = ON;
