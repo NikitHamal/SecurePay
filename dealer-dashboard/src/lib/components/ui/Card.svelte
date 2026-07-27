4fbd6e98e28be5fc5cf4590a8c4e64609567cd3a
Nikit Hamal <iamnikithamal@gmail.com>
2026-07-27 11:47:55 +0545

    up

--- dealer-dashboard/src/lib/components/ui/Card.svelte
+++ dealer-dashboard/src/lib/components/ui/Card.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  export let hover: boolean = true;
+  export let hover: boolean = false;
 </script>
 
-<div class="card {hover ? 'card-hover' : ''} {$$props.class ?? ''}">
+<div class="card {hover ? 'card-hover' : ''} p-5 {$$props.class ?? ''}">
   <slot />
 </div>
