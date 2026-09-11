be51527f3580339fad5caa1e2199c3fcb6a45f62
HostWebs <maurasmith254@gmail.com>
2026-09-11 19:04:48 +0000

    chore: bump version to 1.8.2 (versionCode 33) for release update

--- customer-app/app/build.gradle.kts
+++ customer-app/app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.touchbase.securepay.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 32
-        versionName = "1.8.1"
+        versionCode = 33
+        versionName = "1.8.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
