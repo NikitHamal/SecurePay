1420eb6012c4773a6c67054c0fd6bec02bf669da
HostWebs <maurasmith254@gmail.com>
2026-09-15 10:48:03 +0000

    chore: bump version to 1.8.3 (versionCode 34) for power + Play Store fixes

--- customer-app/app/build.gradle.kts
+++ customer-app/app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.touchbase.securepay.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 33
-        versionName = "1.8.2"
+        versionCode = 34
+        versionName = "1.8.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
